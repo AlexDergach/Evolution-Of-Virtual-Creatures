1ac71e7541ac2bf9cca69283b4df04533c3ea9ea
AlexDergach <dergachalex12@gmail.com>
2023-12-08 19:02:58 +0000

    Changed File location

--- Scripts/preset.gd
+++ Scripts/preset.gd
@@ -4,4 +4,4 @@ func _on_volume_pressed():
 	pass # Replace with function body.
 
 func _on_back_pressed():
-	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	get_tree().change_scene_to_file("res://menu.tscn")
